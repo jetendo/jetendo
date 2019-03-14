f91d4a0823a65ec7ced6dc661dac916e58623dee
Bruce Kirkpatrick <bruce@farbeyondcode.com>
2019-03-14 16:30:37 -0400

    fix km to miles

--- public/javascript/jetendo/map-functions.js
+++ public/javascript/jetendo/map-functions.js
@@ -775,28 +775,38 @@ var zArrGeolocationWatchCallback=[];
 		zGeocodeCacheAddress();
 	}
 
+	// faster
+	function zGetDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
+	  var p = 0.017453292519943295;    // Math.PI / 180
+	  var c = Math.cos;
+	  var a = 0.5 - c((lat2 - lat1) * p)/2 + 
+	          c(lat1 * p) * c(lat2 * p) * 
+	          (1 - c((lon2 - lon1) * p))/2;
 
-
-	// lat/long distance functions 
-	function zGetDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
-		var R = 6371; // Radius of the earth in km
-		var dLat = zDeg2Rad(lat2-lat1);  // deg2rad below
-		var dLon = zDeg2Rad(lon2-lon1); 
-		var a = 
-		Math.sin(dLat/2) * Math.sin(dLat/2) +
-		Math.cos(zDeg2Rad(lat1)) * Math.cos(zDeg2Rad(lat2)) * 
-		Math.sin(dLon/2) * Math.sin(dLon/2)
-		; 
-		var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
-		var d = R * c; // Distance in km
-		return d;
+	  return 12742 * Math.asin(Math.sqrt(a)); // 2 * R; R = 6371 km
 	}
+ 
+ 
+	// lat/long distance functions - old version is same
+	// function zGetDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+	// 	var R = 6371; // Radius of the earth in km
+	// 	var dLat = zDeg2Rad(lat2-lat1);  // deg2rad below
+	// 	var dLon = zDeg2Rad(lon2-lon1); 
+	// 	var a = 
+	// 	Math.sin(dLat/2) * Math.sin(dLat/2) +
+	// 	Math.cos(zDeg2Rad(lat1)) * Math.cos(zDeg2Rad(lat2)) * 
+	// 	Math.sin(dLon/2) * Math.sin(dLon/2)
+	// 	; 
+	// 	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+	// 	var d = R * c; // Distance in km
+	// 	return d;
+	// }
 	function zGetDistanceFromLatLonInMiles(lat1,lon1,lat2,lon2) {
 		var km=zGetDistanceFromLatLonInKm(lat1,lon1,lat2,lon2);
 		return zKmToMiles(km);
 	}
 	function zKmToMiles(km){
-		return km*1.609;
+		return km*0.621371;
 	}
 	function zMilesToKm(miles){
 		return miles*0.621;
@@ -942,5 +952,6 @@ var zArrGeolocationWatchCallback=[];
 	window.zDisplayDirectionsDistance=zDisplayDirectionsDistance;
 	window.zGetDirectionsDistanceByLatLng=zGetDirectionsDistanceByLatLng;
 	window.zGetDirectionsDistanceByAddress=zGetDirectionsDistanceByAddress;
+	window.zGetDistanceFromLatLonInMiles=zGetDistanceFromLatLonInMiles;
 	window.zGeocodeAddress=zGeocodeAddress;
 })(jQuery, window, document, "undefined"); 
